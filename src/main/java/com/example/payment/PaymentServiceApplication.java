daa20c1f8214f8a4925357779ab71a1d2556a6f1
Mauricio Gutiérrez <mauricio.gutierrez.cruz@outlook.com>
2025-04-24 09:12:24 -0600

    feat(app): add main Spring Boot application entrypoint

--- src/main/java/com/example/payment/PaymentServiceApplication.java
+++ src/main/java/com/example/payment/PaymentServiceApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
+        
     }
 }
